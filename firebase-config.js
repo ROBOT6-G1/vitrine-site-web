7388c88996a7bc23c61a9a20937bcff6bc2e5860
ROBOT6-G1 <b.outiquemevasoa@gmail.com>
2026-07-27 08:17:08 +0300

    Update firebase-config.js via DEVWEBIA

---
+++ firebase-config.js
@@ -0,0 +1,3 @@
+// Fanamarihana: Mampiasa LocalStorage fotsiny ity tranonkala ity amin'izao fotoana izao.
+// Raha te hampifandray Firebase ianao, azonao atao ny manitsy ity rakitra ity sy manampy ny apiKey-nao.
+console.log('DEVWEBIA: LocalStorage active mode enabled.');
